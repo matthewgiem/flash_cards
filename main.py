ce8eb398fd045944ad2b8e8664242674ea2fe1ab
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-06-05 16:36:33 +0700

    when you press the check button the word changes to a random french word

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from tkinter import *
 import pandas as pd
+import random
 
 # Constants
 BACKGROUND_COLOR = "#B1DDC6"
 
 # Import data as a PD dataframe
 data = pd.read_csv("data/french_words.csv")
+list_of_words = data.to_dict(orient="records")
+
+# Logic
+def change_word():
+    current_card = random.choice(list_of_words)
+    canvas.itemconfig(card_word, text=current_card["French"])
 
 # Window
 window = Tk()
@@ -21,17 +28,18 @@ wrong = PhotoImage(file="images/wrong.png")
 canvas.create_image(400, 263, image=front_flash_card_image)
 canvas.grid(row=0, column=0, columnspan=2)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Arial", 60 ,"bold"))
+card_title = canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="word", font=("Arial", 60 ,"bold"))
 
 # Buttons
 unknown_button = Button(image=wrong)
 unknown_button.grid(row=1, column=0)
-unknown_button.config(highlightthickness=0)
+unknown_button.config(highlightthickness=0, bg=BACKGROUND_COLOR, command=change_word)
+
 correct_button = Button(image=correct)
 correct_button.grid(row=1, column=1)
-correct_button.config(highlightthickness=0,bg=BACKGROUND_COLOR)
+correct_button.config(highlightthickness=0, bg=BACKGROUND_COLOR, command=change_word)
 
 
-print(data.head())
+print(list_of_words)
 window.mainloop()
