fd173d3d58f8ddddbfbabbb9d88478f38564d426
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-06-05 16:55:21 +0700

    change the cards background image when flipped and text color

--- main.py
+++ main.py
@@ -8,17 +8,27 @@ BACKGROUND_COLOR = "#B1DDC6"
 # Import data as a PD dataframe
 data = pd.read_csv("data/french_words.csv")
 list_of_words = data.to_dict(orient="records")
+current_card = {}
 
 # Logic
 def change_word():
+    global current_card
     current_card = random.choice(list_of_words)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_background_image, image=front_flash_card_image)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background_image, image=back_flash_card_image)
 
 # Window
 window = Tk()
 window.title("Flash Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+window.after(3000, flip_card)
 
 # Canvas
 canvas = Canvas(width=800, height=526)
@@ -26,7 +36,7 @@ front_flash_card_image = PhotoImage(file="images/card_front.png")
 back_flash_card_image = PhotoImage(file="images/card_back.png")
 correct = PhotoImage(file="images/right.png")
 wrong = PhotoImage(file="images/wrong.png")
-canvas.create_image(400, 263, image=front_flash_card_image)
+card_background_image = canvas.create_image(400, 263, image=front_flash_card_image)
 canvas.grid(row=0, column=0, columnspan=2)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 card_title = canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
